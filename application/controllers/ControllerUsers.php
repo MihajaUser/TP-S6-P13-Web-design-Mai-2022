316645bade844457e913d0361cd0dfab8c993e91
MihajaUser <randrianarisoalalatianaantonio@gmail.com>
2022-05-15 13:23:19 +0300

    update

--- application/controllers/ControllerUsers.php
+++ application/controllers/ControllerUsers.php
@@ -7,19 +7,20 @@ class ControllerUsers extends CI_Controller
     {
         parent::__construct();
         $this->load->model('UsersMod');
+        $this->load->model('InfoMod');
     }
     public function accueille()
     {
-        // $user =  $this->UsersMod->checkUsers($this->input->post('email'), sha1($this->input->post('mdp')));
-        // if (count($user) == 0) {
-        //     $data['errorLogin'] = 1;
-        //     $this->load->view('frontoffice/index',$data);
-        // } else {
-        //     $data['page'] = 'acceuille.php';
-        //     $this->session->set_userdata('user', $user);
-        //    // $this->load->view('template', $data);
-             $this->load->view('backoffice/index');
-        // }
+        $user =  $this->UsersMod->checkUsers($this->input->post('email'), sha1($this->input->post('mdp')));
+        if (count($user) == 0) {
+            $data['errorLogin'] = 1;
+            $data['informations']=	$this->InfoMod->getInformation();
+            $this->load->view('frontoffice/index',$data);
+        } else {
+            $data['page'] = 'acceuille.php';
+            $this->session->set_userdata('user', $user);
+            $this->load->view('backoffice/index');
+        }
       
     }
 }
